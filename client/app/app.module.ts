116835eef9814f94db65b67461470ad87961c37c
Emmett Miller <emmett.miller93@gmail.com>
2020-09-17 20:53:06 -0700

    work on very prettier ui

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -5,28 +5,48 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatSnackBarModule, MatToolbarModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatIconModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatListModule
+} from '@angular/material';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { LinkedinLinkComponent } from './linkedin-link/linkedin-link.component';
 import { GithubLinkComponent } from './github-link/github-link.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { AttributionComponent } from './attribution/attribution.component';
+import { HomeLinkComponent } from './home-link/home-link.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
     LinkedinLinkComponent,
-    GithubLinkComponent
+    GithubLinkComponent,
+    FooterComponent,
+    HeroComponent,
+    AttributionComponent,
+    HomeLinkComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
     MatToolbarModule,
     MatButtonModule,
     MatDialogModule,
     MatCardModule,
     MatSnackBarModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/app/hero/hero.component.ts
@@ -0,0 +1,9 @@
+import { ViewportRuler } from '@angular/cdk/overlay';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss']
+})
+export class HeroComponent { }
